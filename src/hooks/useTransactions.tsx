21ff28b2ff30ce84bc228cc5105749b2cc45341c
levircesar <levirlemos@gmail.com>
2021-11-09 10:44:49 -0300

    criando hook

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import { createContext, useEffect, useState, ReactNode } from 'react'
-import { api } from './services/api';
+import { createContext, useEffect, useState, ReactNode, useContext } from 'react'
+import { api } from '../services/api';
 
 
 interface Transaction {
@@ -32,7 +32,7 @@ interface TransactionsContextData {
 
 type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>
 
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
   {} as TransactionsContextData
 );
 
@@ -62,4 +62,10 @@ export function TransactionsProvider({ children }: TransactionsProviderProps){
       {children}
     </TransactionsContext.Provider>
   )
+}
+
+export function useTransactions(){
+  const context = useContext(TransactionsContext);
+
+  return context;
 }
